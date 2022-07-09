f1f373e26be0821d63f52399102f0901af7ad79a
SinaMAmini <smobasheramini@gmail.com>
2022-07-09 19:52:56 +0430

    [ADD] question 04

--- src/interview/question-04/main.js
+++ src/interview/question-04/main.js
@@ -1,25 +1,27 @@
 const box = document.querySelector('.box');
 
-let x = Number.parseInt(box.style.getPropertyValue('--x'));
-let y = Number.parseInt(box.style.getPropertyValue('--y'));
-
-
-
 const initializeEventListeners = () => {
     document.addEventListener('keyup', (e) => {
-        if (e.code === 'KeyS') {
-            y += 100;
-            box.style.setProperty('--y', `${y}px`);
-        } else if (e.code === 'KeyW') {
-            y -= 100;
-            box.style.setProperty('--y', `${y}px`);
-        } else if (e.code === 'KeyA') {
-            x -= 100;
-            box.style.setProperty('--x', `${x}px`);
-        } else if (e.code === 'KeyD') {
-            x += 100;
-            box.style.setProperty('--x', `${x}px`);
+        let x = Number.parseInt(box.style.getPropertyValue('--x'));
+        let y = Number.parseInt(box.style.getPropertyValue('--y'));
+
+        switch (e.code) {
+            case 'KeyS':
+                y += 100;
+                break;
+            case 'KeyW':
+                y -= 100;
+                break;
+            case 'KeyA':
+                x -= 100;
+                break;
+            case 'KeyD':
+                x += 100;
+                break;
         }
+
+        box.style.setProperty('--x', `${x}px`);
+        box.style.setProperty('--y', `${y}px`);
     });
 };
 
